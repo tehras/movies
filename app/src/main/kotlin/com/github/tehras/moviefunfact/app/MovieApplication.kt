ce5d641176e4fbf137ffefc1f3326698166bad38
Taras Koshkin <tkoshkin@gmail.com>
2018-08-30 23:43:12 -0700

    Added JKS and AppCenter

--- app/src/main/kotlin/com/github/tehras/moviefunfact/app/MovieApplication.kt
+++ app/src/main/kotlin/com/github/tehras/moviefunfact/app/MovieApplication.kt
@@ -7,6 +7,9 @@ import com.github.tehras.dagger.components.ComponentProvider
 import com.github.tehras.dagger.components.DaggerApplication
 import com.github.tehras.logger.BuildConfig
 import com.github.tehras.logger.CrashReportingTree
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import ext.android.content.isDarkModeEnabled
 import timber.log.Timber
 import timber.log.Timber.DebugTree
@@ -48,5 +51,13 @@ class MovieApplication : Application(), DaggerApplication, ComponentProvider<App
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
+
+        // App Center
+        AppCenter.start(
+                this,
+                "f205ec00-3465-4dbb-a65f-6fd65c4a9108",
+                Analytics::class.java,
+                Crashes::class.java
+        )
     }
 }
